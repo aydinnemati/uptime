9f2dffa58763554bb99addcde3d889a63c1a2587
aydinnemati <aydinnemati199888@gmail.com>
2021-12-27 12:55:53 +0330

    refactor

--- app/app.js
+++ app/app.js
@@ -3,50 +3,81 @@ const hostsModel = require('./models/hostsModel');
 const nodesModel = require('./models/nodesModel');
 const configGenerator = require('./functions/configGenerator');
 const fs = require('fs');
-const {Docker} = require('node-docker-api');
- 
-const docker = new Docker({ socketPath: '/var/run/docker.sock' });
+// const {Docker} = require('node-docker-api');
+// const docker = new Docker({ socketPath: '/var/run/docker.sock' });
+
+
+const Dockerode = require('dockerode');
+const DockerodeCompose = require('dockerode-compose');
+
 
 require('dotenv').config()
 
 const schema = {
-    body: {
-        'nodes': nodesModel,
-        'hosts': hostsModel,
-    }
+  body: {
+    'nodes': nodesModel,
+    'hosts': hostsModel,
+  }
 }
 
 
-fastify.get('/', (req, reply) => {
-    reply.send(fs.readFileSync(process.env.current_config_file, err => {
-        if (err) {
-          console.error(err)
-          return
-        }
-    }))
-})
+// fastify.get('/', (req, reply) => {
+//     reply.send(fs.readFileSync(process.env.current_config_file, err => {
+//         if (err) {
+//           console.error(err)
+//           return
+//         }
+//     }))
+// })
 
 fastify.get('/test', (req, reply) => {
-    reply.send('its good im here')
+  reply.send('its good im here')
 })
 
-fastify.post('/', { schema }, (req, reply) => {
-    const newConfig = configGenerator(req.body);
+const docker = new Dockerode();
+const container = docker.getContainer('prometheus');
+const compose = new DockerodeCompose(docker, '/home/lucifer/Desktop/Hell/dockersss/prometheus/docker-compose.yml', 'poj-name');
 
-    fs.rename( process.env.current_config_file, process.env.logged_config_file , async (err) => {
-        if (err) throw err;
-        // const newconfig = await configGenerator(req.body) //
-        fs.writeFile(process.env.current_config_file, newConfig, err => {
-        if (err) {
-          console.error(err)
-          return
-            }
-        docker.container.list()
-           .then(containers => containers[0].restart()) // prometheus container must be the first
-           .catch(error => console.log(error));
-        })
-    });
-    reply.send(newConfig);
+fastify.post('/', { schema }, (req, reply) => {
+  container.stop(function (err, data) {
+    if (err) { 
+      console.error(err);
+        throw err;
+    }
+      console.log(data);
+
+
+      container.remove(function (err, data) {
+        if (err) { 
+          console.error(err);
+            throw err;
+        }
+          console.log(data);
+
+
+
+          const newConfig = configGenerator(req.body);
+          fs.rename(process.env.current_config_file, process.env.logged_config_file, async (err) => {
+            if (err)
+              throw err;
+              fs.writeFile(process.env.current_config_file, newConfig, async (err) => {
+                if (err) {
+                  console.log(err);
+                  throw err;
+                }
+                  await compose.pull();
+                  let state = await compose.up();
+                  console.log(state);
+                  reply.send(newConfig);
+
+              });
+          });
+
+
+
+      });
+    });      
+
 })
 
 module.exports = fastify;
---
+++ docs/implement.md
@@ -0,0 +1,113 @@
+# implement monitoring UPTIME ecosystem
+
+## requirements
+- prometheus server
+- blackbox node(s) => [2 nodes in this case]
+- docker & docker-compose on each host(prometheus and node(s))
+
+## deployment
+
+
+> ## prometheus server
+- first of all deploy prometheus server with integration of ansible and config generator application written in nodeJS
+
+> 1. install prometheus
+- pull docker images [docker.com](https://hub.docker.com/r/prom/prometheus)
+```bash
+$ docker pull prom/prometheus:main
+```
+- create __docker-compose.yml__
+```yaml
+version: '3'
+
+services:
+  prometheus:
+    image: prom/prometheus:main
+    container_name: prometheus
+    ports:
+      - 9000:9090
+    volumes:
+      - ./prometheus.yml:/etc/prometheus/prometheus.yml # will create this config file later
+      - prometheus-data:/prometheus
+    command: --web.enable-lifecycle  --config.file=/etc/prometheus/prometheus.yml
+
+volumes:
+  prometheus-data:
+```
+- finally with sample config file below getting prometheus up
+```yaml
+# sample config file just for getting no errors
+global:
+  scrape_interval:     15s # By default, scrape targets every 15 seconds.
+
+  # Attach these labels to any time series or alerts when communicating with
+  # external systems (federation, remote storage, Alertmanager).
+  external_labels:
+    monitor: 'codelab-monitor'
+
+# A scrape configuration containing exactly one endpoint to scrape:
+# Here it's Prometheus itself.
+scrape_configs:
+  # The job name is added as a label `job=<job_name>` to any timeseries scraped from this config.
+  - job_name: 'prometheus'
+
+    # Override the global default and scrape targets from this job every 5 seconds.
+    scrape_interval: 5s
+
+    static_configs:
+      - targets: ['localhost:9090']
+```
+```bash
+$ docker-compose up -d
+```
+> 2. installing ansible
+
+**_NOTE: you can also install ansible directly via apt_**
+
+- install ansible using pip but first should install pip with apt
+```bash
+$ sudo apt -y install python3-pip
+```
+- then installing __ansible__ using __pip__
+```bash
+$ sudo pip3 install ansible
+```
+- then create __hosts__ file
+```
+xxx.xxx.xxx.xxx # node01 IP
+xxx.xxx.xxx.xxx # node02 IP
+etc...
+```
+- copy blackbox ansible files in ansible directory
+- install __sshpass__ using apt
+```bash
+$ sudo apt install sshpass
+```
+- run ansible playbook with command below to run BB nodes
+```bash
+$ ansible-playbook -u a -i ./hosts blackbox_install.yml -Kk
+```
+- if ansible playbook can not pull blackbox image you can pull it directly
+```
+$ docker pull prom/blackbox-exporter:master
+```
+
+> 3. run config generator app for passing nodes & hosts to get configuration of prometheus
+- download app into configapp directory
+```bash
+$ wget https://github.com/aydinnemati/uptime/archive/refs/heads/main.zip
+```
+- install zip and unzip main.zip
+```bash
+$ sudo apt -y install zip
+$ unzip main.zip
+```
+- navigate in app directory and build app
+```bash
+$ cd ~/configapp/uptime-main/app
+$ docker build -t configapp .
+```
+- run app
+```
+$ docker-compose up -d
+```
